c9414edf9e18dca19796ef4c64bce4caee993cb9
lundeg <lundegee@gmail.com>
2019-04-07 15:48:19 +0800

    section 18 - Intro JQuery Completed

---
+++ Section 18 - intro_JQuery/exercise.js
@@ -0,0 +1,11 @@
+//buh divin backgroundig purple bolgo
+$("div").css("background","purple");
+
+//.highlight classin orgonig 200px bolgo
+$(".highlight").css("width","200px");
+
+//third idg border orange bolgo
+$("#third").css("border","2px solid orange");
+
+// ehni divin songoj avaad colorig n pink bolgo
+$("div:first-of-type").css("color","pink");
